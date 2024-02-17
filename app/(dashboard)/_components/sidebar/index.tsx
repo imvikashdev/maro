04874310442bbf68c6183e2b85b51c11c9ac2150
Vikash Choudhary <imvikashhereyo@gmail.com>
2024-02-17 21:23:51 +0530

    side bar added

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import NewButton from './NewButton';
+import { List } from './List';
 
 type Props = {};
 
 const Sidebar = (props: Props) => {
   return (
     <aside className="fixed text-white z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4">
-      Side
+      <List />
+      <NewButton />
     </aside>
   );
 };
